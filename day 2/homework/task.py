bd391809d0c89ec5f2c3fe4f53f0bbf947e9fb3c
gabrieldaraselia-coder <gabriel.daraselia18@gmail.com>
2026-02-21 22:28:42 +0400

    day2

---
+++ day 2/homework/task.py
@@ -0,0 +1,21 @@
+number1 = 3
+number2 = 6
+print(number1+number2)
+
+number1 = 4
+number2 = 15
+print(number1 * number2)
+
+number1 = 5
+number2 = 16
+number3 = 20
+number4 = 94
+number5 = 18
+print(number1 +number2 +number3 + number4 + number5)
+print((number1 +number2 +number3 + number4 + number5)/5)
+
+# Declaration - მინიᲭების ოპერატორი
+# Initialization - ცვლადის Შექმნა მნიᲨვნრლობის მინიᲭება
+# Reinitialization - ცვლადზე მნიᲨვნელობის Შეცვლა
+# Concetanation - ცვლადის გამოყენებიᲗ წინადადების აგება
+# debugging - შეცდომების გასწორება
