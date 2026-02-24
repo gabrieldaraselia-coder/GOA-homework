c3ca52773b023e242f9dce56b8a8c217f80e145c
gabrieldaraselia-coder <gabriel.daraselia18@gmail.com>
2026-02-24 20:13:20 +0400

    day 6

---
+++ day 6 lesson/lesson.py
@@ -0,0 +1,10 @@
+print(type(7))
+
+print(type("gabo"))
+
+print(type(3.14))
+
+
+name = input(' enter your name: ') #gabo
+
+print(type(name))
---
+++ day 6/homework/task.py
@@ -0,0 +1,24 @@
+# მონაცემთა ტიპი მაგალითად სტრინგი ინტენჯერი და ფლოატი უფრო გვიადვილებს კოდის წერაში და მათმატიკური ამოცანების გამოთვლაში გვეხმარება
+
+name = input("enter your name here: ")
+surname = input("enter your surname here: ")
+
+Name = "gabrieli"
+weight = 38
+height = 15.7
+
+
+print(type(Name))
+print(type(weight))
+print(type(height))
+
+distance = float(236)
+print(type(distance))
+print(236 * 100)
+
+print(23+24535)
+print(24/2)
+print(55*5)
+print(957-567)
+
+# BMI = 15.4 kg/m2 
