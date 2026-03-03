a0f038dde51e61ed2e7eab0706e36b573e2e6163
gabrieldaraselia-coder <gabriel.daraselia18@gmail.com>
2026-03-03 23:11:43 +0400

    day 8

---
+++ day 7/classwork/task.py
@@ -0,0 +1,3 @@
+age = input("Enter your age here: ")
+is_adult= 13 > 18
+print(type(is_adult))
---
+++ day 8/homework/task.py
@@ -0,0 +1,69 @@
+#string
+print(type("gabrieli"))
+print(type("goga"))
+print(type("likuna"))
+print(type("nona"))
+print(type("magda"))
+
+#intenger
+print(type(13))
+print(type(41))
+print(type(5))
+print(type(98))
+print(type(23))
+
+
+#float
+print(type(34.5))
+print(type(56.1))
+print(type(76.6))
+print(type(23.6))
+print(type(89.1))
+
+#მათემატიკური ოპერატორები
+# / + - // * ** % 
+
+#შედარების ოპერატორები
+# >
+# <
+# >=
+# <=
+
+print(10>22)
+print(11>75)
+print(13>1)
+print(43>33)
+
+print(23<123)
+print(43<33)
+print(14<43)
+print(24<44)
+print(26<56)
+
+print(68 >=100)
+print(45 >=33)
+print(69 >=345)
+print(47 >=76)
+print(22 >=44)
+
+print(54<=2)
+print(75<=56)
+print(23<=156)
+print(90<=91)
+
+#ლოგიკური ოპერატორები
+
+robot_cleaner = 68
+is_robot_cleaner = robot_cleaner > 100
+print(is_robot_cleaner)
+
+
+number = 15
+print(number>10)
+print(number>20)
+
+
+
+Number = 164
+print(Number>100)
+print(Number>999)
