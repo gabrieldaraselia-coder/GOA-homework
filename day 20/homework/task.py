5ccf5ef0e3b547be83e8553c53b7ae19f0d33319
gabrieldaraselia-coder <gabriel.daraselia18@gmail.com>
2026-04-11 22:07:35 +0400

    day 20

--- day 20/homework/task.py
+++ day 20/homework/task.py
@@ -1,59 +1,60 @@
 # შექმენი სია, სადაც შეინახავ შენს საყვარელ ფერებს.
-Colours = ["red", "blue", "black"]
-print(Colours)
+colours = ["red", "blue", "black"]
+print(colours)
 
 # შექმენი სია, სადაც შეინახავ კვირის დღეებს.
 Week_Days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
 # შექმენი სია, სადაც შეინახავ 5 სხვადასხვა რიცხვს. -> დაბეჭდე მესამე ინდექსზე მყოფი ელემენტი
 
-Numbers = [15, 16, 17, 18, 19, 20]
-if Numbers == 18:
-    Numbers[3] = 21
-print(Numbers)
+numbers = [15, 16, 17, 18, 19, 20]
+numbers[4] = 21
+print(numbers)
 
 
 
 
 # შექმენი სია, სადაც შეინახავ 5 ქალაქის სახელს. -> შეცვალე მეორე ინდექსზე მყოფი ელემენტი სხვა ქალაქით
-Cities = ["batumi", "zugdidi", "tbilisi", "berlin", "moskow"]
-if Cities[2] == "tbilisi":
-    Cities[2] = "qutaisi"
-print(Cities)
+cities = ["batumi", "zugdidi", "tbilisi", "berlin", "moskow"]
+cities[2] = "qutaisi"
+print(cities)
 
 
 # შექმენი სია, სადაც შეინახავ შენი ოჯახის წევრების სახელებს. -> დაბეჭდე პირველი და ბოლო ელემენტი (ინდექსებით)
-# es ver gavige
+family = ["salome", "giga", "magda", "likuna", "nona", "fati", "avto", "tamazi"]
+print(family[0])
+print(family[7])
 
 # შექმენი სია, სადაც შეინახავ 6 სხვადასხვა რიცხვს. -> შეცვალე მეოთხე ინდექსზე მყოფი ელემენტი 100-ით
 numbers = [21, 22, 23, 24, 25, 26]
-if numbers[4] == 25:
-    numbers[4] = 100
+numbers[4] = 100
 print(numbers)
 
 
 
 # შექმენი სია, სადაც შეინახავ 10 ძაღლის სახელს. -> შეცვალე მეხუთე ელემენტი სხვა თამაშით
-Dog_Names = ["Otis", "Ace", "Finn", "Gus", "Ben", "Gizmo", "Blue", "Bo", "Jax", "Hank"]
-if Dog_Names[5] == "Gizmo":
-    Dog_Names[5] = "Roblox"
-print(Dog_Names)
+dog_Names = ["Otis", "Ace", "Finn", "Gus", "Ben", "Gizmo", "Blue", "Bo", "Jax", "Hank"]
+dog_Names[4] = "Roblox"
+print(dog_Names)
 
 
 # შექმენი სია, სადაც შეინახავ 3 სტრინგს, 3 რიცხვს და 3 ბულეანს. -> დაბეჭდე ნებისმიერი ერთი ელემენტი ინდექსის გამოყენებით
-Attributs = ["cat", "car", "cow", 15, 16, 17, True, False, True]
-print(Attributs[2])
+data_types = ["cat", "car", "cow", 15, 16, 17, True, False, True]
+print(data_types[2])
 
 
 # შექმენი სია, სადაც შეინახავ 5 მუსიკოსის სახელს. -> თუ პირველი ელემენტი არის 'Drake' -> შეცვალე სხვა მუსიკოსით
-Musisikians = ["mozart", "Drake", "led zeppelin", "michael jackson", "taylor swift"]
-if Musisikians[1] == "Drake":
-    Musisikians[1] = "bruno mars"
-print(Musisikians)
+musisikians = ["Drake", "mozart", "led zeppelin", "michael jackson", "taylor swift"]
+if musisikians[0] == "Drake":
+    musisikians[0] = "bruno mars"
+print(musisikians)
 
 
 # შექქმენი სია, სადაც შეინახავ რიცხვებს(როგორც მთელები ისე არამთელები) -> გაზარდე სიის პირველი და ბოლო ელემენტი 1000-ით
-#es ver gavige
+numbers = [3, 3.14, 5, 7.89, 10]
+numbers[0] = numbers[0] + 1000
+numbers[4] = numbers[4] + 1000
+print(numbers)
 
 
 # შექმენი სია, სადაც შეინახავ 3 რიცხვს(როგორც მთელები ისე არამთელები)
@@ -61,4 +62,9 @@ print(Musisikians)
 #       გაზარდე სიის მესამე ელემენტი პირველი და მეორე ელემენტის ნამრავლით
 #   - სხვა შემთხვევაში:
 #       გაზარდე სიის პირველი ელემენტი მეორე და მესამე ელემენტის ნამრავლით
-# es ver gavige
+numbers = [9, 15, 23.50]
+if numbers[0] > 10:
+    numbers[2] = numbers[2] + numbers[0] * numbers[1]
+else:
+    numbers[0] = numbers[0] + numbers[2] * numbers[1]
+print(numbers)
